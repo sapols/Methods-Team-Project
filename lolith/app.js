f1530ccf260e0d61b10ade835c54fbdda20cb4d8
Shawn <sapols@yahoo.com>
2016-02-23 19:33:39 -0700

    Added a Procfile for Heroku.

--- lolith/app.js
+++ lolith/app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var port = process.env.PORT || 3001;
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -29,7 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 
-app.listen(3001);
+app.listen(port);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
