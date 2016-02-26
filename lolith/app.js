79504d5a320edca2fde8f70c1d2fb90a20e28658
Shawn Polson <sapols@yahoo.com>
2016-02-26 10:58:49 -0700

    Update app.js

--- lolith/app.js
+++ lolith/app.js
@@ -25,7 +25,12 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public'))); //Old version
+// At the top of your web.js
+process.env.PWD = process.cwd()
+
+// Then
+app.use(express.static(process.env.PWD + '/htdocs'));
 
 app.use('/', routes);
 app.use('/users', users);
