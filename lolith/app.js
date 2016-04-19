5edb54495d3f8f9a5679af1e1e028c79d573d7af
amjaad <amjad.alharbi@colorado.edu>
2016-04-19 09:35:16 -0600

    Fixed signUp POST request

--- lolith/app.js
+++ lolith/app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 var port = process.env.PORT || 3001;
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
@@ -28,15 +29,22 @@ app.set('view engine', 'html');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, 'public'))); //Old version
+app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public'))); //Old version
-
-// At the top of your web.js
-//process.env.PWD = process.cwd()
-
+app.use(logger('dev'));
+app.use(session({
+  secret: 'password',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true }
+}));
+//Initialize Passport and restore authentication state
+app.use(passport.initialize());
+app.use(passport.session());
+app.use('/', routes);
+app.use('/users', users);
 
 
 // use static authenticate method of model in LocalStrategy
@@ -46,18 +54,17 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 // Connect mongoose
-// mongoose.connect('mongodb://localhost/Lolith', function(err) {
-//   if (err) {
-//     console.log('Could not connect to mongodb on localhost. Ensure that you have mongodb running on localhost and mongodb accepts connections on standard ports!');
-//   }
-// });
-//Initialize Passport and restore authentication state
-app.use(passport.initialize());
-app.use(passport.session());
+mongoose.connect('mongodb://localhost/Lolith', function(err) {
+  if (err) {
+    console.log('Could not connect to mongodb on localhost. Ensure that you have mongodb running on localhost and mongodb accepts connections on standard ports!');
+  }
+});
+
 
 // Define routes.
 app.get('/login_signup', function(req, res) {
-  res.sendfile('views/login_signup.html');
+  // res.sendfile('views/login_signup.html')
+  res.render('login_signup', { title: 'Login' });
 });
 
 app.post('/login',
@@ -67,12 +74,12 @@ app.post('/login',
   })
 );
 
-  app.post('/register', function(req, res, next) {
-  console.log('registering user');
-  user.register(new User({username: req.body.username}), req.body.password, function(err) {
+  app.post('/signup', function(req, res, next) {
+  console.log('registering user'+ req.body.email);
+  User.register(new User({username: req.body.username}), req.body.password, function(err,account) {
     if (err) {
       console.log('error while user register!', err);
-      return next(err);
+      return res.render('login_signup', { account : account });
     }
 
     console.log('user registered!');
@@ -86,8 +93,7 @@ app.post('/login',
 // Then
 //app.use(express.static(process.env.PWD + '/htdocs'));
 
-app.use('/', routes);
-app.use('/users', users);
+
 
 app.listen(port);
 
